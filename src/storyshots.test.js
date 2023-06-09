eeb84d126e4541dcb1e84afb2a766ea1072b26b1
Matthew Jiao <matthew.jiao@stackadapt.ca>
2023-06-09 12:17:27 -0400

    snapshots

--- src/storyshots.test.js
+++ src/storyshots.test.js
@@ -1,28 +1,3 @@
 import initStoryshots from '@storybook/addon-storyshots';
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
-import puppeteer from 'puppeteer';
-
-const beforeScreenshot = async (page) => {
-  // Unregister all service workers before taking a snapshot
-  await page.evaluate(() => {
-    return navigator.serviceWorker.getRegistrations()
-      .then(registrations => {
-        for(let registration of registrations) {
-          registration.unregister();
-        }
-      });
-  });
-
-  return page;
-};
-
-initStoryshots({
-  suite: 'Image storyshots',
-  test: imageSnapshot({
-    storybookUrl: 'http://localhost:6008',
-    beforeScreenshot,
-    getCustomBrowser: () => puppeteer.launch({
-      ignoreHTTPSErrors: true, // Ignore HTTPS errors
-    }),
-  }),
-});
+initStoryshots({suite: 'Image storyshots', test: imageSnapshot({storybookUrl: 'http://localhost:6008'})});
