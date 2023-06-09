0c40aaf02da7d18efe6c85d5da72ac09297d047c
Matthew Jiao <matthew.jiao@stackadapt.ca>
2023-06-09 12:37:53 -0400

    snapshots

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -14,7 +14,7 @@ export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
-      {label} Storyshots Test 2
+      {label} Storyshots Test 3
     </button>
   );
 };
