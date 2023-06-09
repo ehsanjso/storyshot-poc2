364a35fd3ede00ea9f0c2088d2f4d3a65494738f
Matthew Jiao <matthew.jiao@stackadapt.ca>
2023-06-09 12:21:06 -0400

    snapshots

--- src/storyshots.test.js
+++ src/storyshots.test.js
@@ -1,3 +1,16 @@
 import initStoryshots from '@storybook/addon-storyshots';
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
-initStoryshots({suite: 'Image storyshots', test: imageSnapshot({storybookUrl: 'http://localhost:6008'})});
+
+const getScreenshotOptions = () => {
+  return {
+    delay: 500, // delay in milliseconds
+  };
+};
+
+initStoryshots({
+  suite: 'Image storyshots', 
+  test: imageSnapshot({
+    storybookUrl: 'http://localhost:6008',
+    getScreenshotOptions,
+  }),
+});
